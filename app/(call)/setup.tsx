1efde38b79f6834057413371f110a39d7941c879
aycom366 <bamigboyeayomide200@gmail.com>
2024-06-05 17:18:31 +0100

    revert app.json to config from json

--- app/(call)/setup.tsx
+++ app/(call)/setup.tsx
@@ -1,3 +1,19 @@
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+import { useState } from "react";
+
 export default function Page() {
-  return <></>;
+  const [isInProgress, setInProgress] = useState(false);
+  return (
+    <>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => {
+          // initiate sign in
+        }}
+        disabled={isInProgress}
+      />
+      ;
+    </>
+  );
 }
